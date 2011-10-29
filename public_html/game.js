55c31b27b0801d5678ab6da1d40c0461c03afee6
hanachin <hanachin@gmail.com>
2011-10-29 15:13:05 +0900

    配列のプロパティにshuffleメソッドを追加

--- public_html/game.js
+++ public_html/game.js
@@ -1,4 +1,15 @@
-﻿enchant();
+Array.prototype.shuffle = function() {
+    var i = this.length;
+    while(i){
+        var j = Math.floor(Math.random()*i);
+        var t = this[--i];
+        this[i] = this[j];
+        this[j] = t;
+    }
+    return this;
+}
+
+enchant();
 //test
 var game;
 window.onload = function() {
